d97c43e06b26dc6468094d43d01b98eee2600c22
John Higgins <johnhiggins@outlook.ie>
2018-05-29 14:57:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function happyHolidaysTo(name){
   return "Happy holidays, " + name + "!"
 }
 
-function happyHolidayTo(){
-  
+function happyHolidayTo(holiday,name){
+  return "Happy holidays, " + name + "!"
 }
 
 function holidayCountdown(){
